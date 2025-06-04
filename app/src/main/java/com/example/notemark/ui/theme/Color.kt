74eb47448ef7858f93c45ba281e82d12c5d6d8ed
Nicolas Marin <nicolasmarin1312@gmail.com>
2025-06-04 13:40:16 -0300

    Colors changed

--- app/src/main/java/com/example/notemark/ui/theme/Color.kt
+++ app/src/main/java/com/example/notemark/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.notemark.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Black1 = Color(0xFF1B1B1C)
+val Black2 = Color(0xFF535364)
+val Grey1 = Color(0xFFE4E4E4)
+val Grey2 = Color(0xFFEFEFF2)
+val White = Color(0xFFFFFFFF)
+val Red = Color(0xFFE1294B)
+val Blue = Color(0xFF5977F7)
+val BlueLight = Color(0xEEEEF1FE)
