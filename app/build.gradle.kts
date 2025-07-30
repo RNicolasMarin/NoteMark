93743d4f5555a062c815b949247e8b625e880b38
Nicolas Marin <nicolasmarin1312@gmail.com>
2025-07-30 19:32:18 -0300

    Libraries related to Hilt and Retrofit removed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.daggerHilt)
     alias(libs.plugins.devtoolsKsp)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.kotlin.parcelize)
@@ -80,28 +79,14 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
 
-
-
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
-    implementation(libs.logging.interceptor)
-
-    implementation(libs.gson)
-
     //Koin
     implementation(libs.bundles.koin)
 
-    //hilt
-    implementation(libs.hilt.android)
-    implementation(libs.androidx.hilt.navigation.compose)
-    ksp(libs.hilt.compiler)
-
+    //Ktor
     implementation(libs.bundles.ktor)
 
     // Crypto
     implementation(libs.androidx.security.crypto.ktx)
-
-
 
 
 }
